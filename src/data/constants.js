38052ec3305caee7dc1cdbf1548a9458b23a455c
Hrishi <mehtahrishi45@gmail.com>
2025-03-06 11:20:15 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,562 @@
+export const Bio = {
+  name: "Hrishi Mehta",
+  roles: [
+    "Full Stack Developer",
+    "Software Tester",
+    "DevOps Engineer",
+    "Ethical Hacker",
+    "Automation Tester"
+  ],
+  description:
+"I'm a full-stack developer, software tester, ethical hacker, and cloud DevOps engineer passionate about technology and innovation. I excel at building and securing high-quality applications, from front-end to back-end and beyond. With a growth mindset and a drive for results, I'm ready to tackle any challenge. I'm eager to contribute my skills and make a real difference in a collaborative environment.",
+  github: "https://github.com/mehtahrishi",
+resume:
+    "https://drive.google.com/file/d/19FVqIb9YnCBlmwyX2GGwMQPvxMewhgru/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/hrishi-mehta-889732256/",
+  insta: "https://www.instagram.com/_mehtahrishi/",
+};
+
+export const skills = [
+  {
+    title: "Hacking Tools",
+    skills: [
+      {
+        name: "Nmap",
+        image:
+          "https://img.icons8.com/color/600/nmap.png",
+      },
+      {
+        name: "Wireshark",
+        image:
+          "https://img.icons8.com/?size=100&id=rOHcpTUtCTjr&format=png&color=000000",
+      },
+      {
+        name: "Metasploit",
+        image:
+          "https://img.icons8.com/?size=100&id=PW0ChfedZvTh&format=png&color=000000",
+      },
+      {
+        name: "Burp Suite",
+        image: "https://predatech.co.uk/wp-content/uploads/2021/07/BurpSuite-Pic-2.0-min.png",
+      },
+      {
+        name: "John The Ripper",
+        image:
+          "https://islanddog.ky/wp-content/uploads/2020/10/john.png",
+      },
+      {
+        name: "Hydra",
+        image:
+          "https://hackblue.org/images/hydra-logo.svg",
+      },
+      {
+        name: "Kali Linux",
+        image:
+          "https://www.snel.com/wp-content/uploads/Ontwerp-zonder-titel-3-3.png",
+      },
+    ],
+  },
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React JS",
+        image:
+          "https://cdn.jsdelivr.net/npm/devicon@2.16.0/icons/react/react-original.svg",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "Next JS",
+        image:
+          "https://cdn.jsdelivr.net/npm/devicon@2.16.0/icons/nextjs/nextjs-original.svg",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://cdn.jsdelivr.net/npm/devicon@2.16.0/icons/css3/css3-original.svg",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "TypeScript",
+        image:
+          "https://cdn.jsdelivr.net/npm/devicon@2.16.0/icons/typescript/typescript-original.svg",
+      },
+      {
+        name: "Sass",
+        image:
+          "https://cdn.jsdelivr.net/npm/devicon@2.16.0/icons/sass/sass-original.svg",
+      },
+     
+      {
+        name: "Bootstrap",
+        image:
+          "https://cdn.jsdelivr.net/npm/devicon@2.16.0/icons/bootstrap/bootstrap-original.svg",
+      },
+      { 
+        name: "Tailwind CSS", 
+        image: "https://cdn.jsdelivr.net/npm/devicon@2.16.0/icons/tailwindcss/tailwindcss-original.svg",
+
+       },
+
+      {
+        name: "Post CSS",
+        image:
+          "https://cdn.jsdelivr.net/npm/devicon@2.16.0/icons/postcss/postcss-original.svg",
+      },
+      {
+        name: "Framer Motion",
+        image:
+          "https://cdn.worldvectorlogo.com/logos/framer-motion.svg",
+      },
+    ],
+  },
+  
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "C",
+        image: "https://cdn.jsdelivr.net/npm/devicon@2.16.0/icons/c/c-original.svg",
+      },
+      {
+        name: "C++",
+        image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg",
+      },
+      {
+        name: "Node JS",
+        image: "https://cdn.jsdelivr.net/npm/devicon@2.16.0/icons/nodejs/nodejs-original.svg",
+      },
+
+      {
+        name: "Express JS",
+        image:"https://cdn.jsdelivr.net/npm/devicon@2.16.0/icons/express/express-original.svg",
+      },
+      {
+        name: "Java",
+        image:
+          "https://cdn.jsdelivr.net/npm/devicon@2.16.0/icons/java/java-original.svg",
+      },
+      {
+        name: "PHP",
+        image: "https://cdn.jsdelivr.net/npm/devicon@2.16.0/icons/php/php-original.svg",
+      },
+      {
+        name: "XML",
+        image: "https://cdn.jsdelivr.net/npm/devicon@2.16.0/icons/xml/xml-original.svg",
+      },
+    ],
+  },
+  {
+    title: "Databases",
+    skills: [
+      {
+        name: "Neo4j",
+        image:
+          "https://static-00.iconduck.com/assets.00/neo4j-icon-1808x2048-22lbowan.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://cdn.jsdelivr.net/npm/devicon@2.16.0/icons/mongodb/mongodb-original.svg",
+      },
+      {
+        name: "PostgreSQL",
+        image:
+          "https://cdn.jsdelivr.net/npm/devicon@2.16.0/icons/postgresql/postgresql-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://cdn.jsdelivr.net/npm/devicon@2.16.0/icons/mysql/mysql-original.svg",
+      },
+    ],
+  },
+  {
+    title: "Dev Operations",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://cdn.jsdelivr.net/npm/devicon@2.16.0/icons/git/git-original.svg",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://www.freeiconspng.com/thumbs/github-icon/github-icon-9.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://cdn.jsdelivr.net/npm/devicon@2.16.0/icons/docker/docker-original.svg",
+      },
+      {
+        name: "Kubernetes",
+        image:
+          "https://cdn.jsdelivr.net/npm/devicon@2.16.0/icons/kubernetes/kubernetes-original.svg",
+      },
+      {
+        name: "AWS",
+        image:
+          "https://img.icons8.com/androidL/512/FFFFFF/amazon-web-services.png",
+      },
+      {
+        name: "CI/CD",
+        image:
+        "https://cdn.jsdelivr.net/npm/devicon@2.16.0/icons/githubactions/githubactions-original.svg",
+    },
+      {
+        name: "Vercel",
+        image:
+          "https://registry.npmmirror.com/@lobehub/icons-static-png/latest/files/dark/vercel.png",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://cdn.jsdelivr.net/npm/devicon@2.16.0/icons/netlify/netlify-original.svg",
+      },
+      {
+        name: "Nginx",
+        image:
+          "https://cdn.jsdelivr.net/npm/devicon@2.16.0/icons/nginx/nginx-original.svg",
+      },
+      {
+        name: "GCP",
+        image:
+          "https://cdn.jsdelivr.net/npm/devicon@2.16.0/icons/googlecloud/googlecloud-original.svg",
+      },
+      {
+        name: "Apache",
+        image:
+          "https://cdn.jsdelivr.net/npm/devicon@2.16.0/icons/apache/apache-original.svg",
+      },
+      {
+        name: "Bash",
+        image:
+          "https://cdn.jsdelivr.net/npm/devicon@2.16.0/icons/bash/bash-original.svg",
+      },
+      {
+        name: "Linux",
+        image:
+          "https://cdn.jsdelivr.net/npm/devicon@2.16.0/icons/linux/linux-original.svg",
+      },
+      {
+        name: "Prometheus",
+        image:
+          "https://cdn.jsdelivr.net/npm/devicon@2.16.0/icons/prometheus/prometheus-original.svg",
+      },
+      {
+        name: "Ansible",
+        image:
+          "https://cdn.jsdelivr.net/npm/devicon@2.16.0/icons/ansible/ansible-original.svg",
+      },
+      {
+        name: "Jenkins",
+        image:
+          "https://cdn.jsdelivr.net/npm/devicon@2.16.0/icons/jenkins/jenkins-original.svg",
+      },
+
+      
+    ],
+  },
+  {
+    title: "Testing & AI Tools",
+    skills: [
+      {
+        name: "Selenium",
+        image:
+          "https://cdn.jsdelivr.net/npm/devicon@2.16.0/icons/selenium/selenium-original.svg",
+      },
+      {
+        name: "Playwright",
+        image:
+          "https://playwright.dev/img/playwright-logo.svg",
+      },
+      {
+        name: "Postman",
+        image:
+        "https://cdn.iconscout.com/icon/free/png-256/free-postman-logo-icon-download-in-svg-png-gif-file-formats--technology-social-media-company-brand-vol-5-pack-logos-icons-2945092.png?f=webp&w=256",
+      },
+      {
+        name: "ACCELQ",
+        image:
+          "https://pbs.twimg.com/profile_images/1352555133293391872/vyH2YBzf_400x400.png",
+      },
+      {
+        name: "Espresso",
+        image:
+          "https://res.cloudinary.com/apideck/image/upload/v1561936190/catalog/espresso-testing/icon128x128.png",
+      },
+      {
+        name: "Appium",
+        image:
+          "https://static-00.iconduck.com/assets.00/appium-icon-2044x2048-8eq3vjix.png",
+      },
+      {
+        name: "LambdaTest",
+        image:
+          "https://techbagfrontend.s3-ap-south-1.amazonaws.com/logos/wTuCVuR7naSPAXxd45mC5S.png",
+      },
+      {
+        name: "Gemini AI",
+        image:
+          "https://cdn.freelogovectors.net/wp-content/uploads/2024/02/gemini_logo-chatbot-freelogovectors.net_.png",
+      },
+      {
+        name: "ChatGPT",
+        image:
+          "https://img.icons8.com/?size=192&id=TUk7vxvtu6hX&format=png",
+      },
+      {
+        name: "Co Pilot",
+        image:
+          "https://img.icons8.com/ios_filled/200/FFFFFF/microsoft-copilot.png",
+      },
+    ],
+  },
+  {
+    title: "Data Analysis",
+    skills: [
+      {
+        name: "R",
+        image:
+          "https://cdn.jsdelivr.net/npm/devicon@2.16.0/icons/r/r-original.svg",
+      },
+      {
+        name: "Power BI",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/New_Power_BI_Logo.svg/1200px-New_Power_BI_Logo.svg.png",
+      },
+      {
+        name: "Excel",
+        image:
+          "https://www.freeiconspng.com/thumbs/xls-icon/microsoft-office-mac-tilt-excel-icon-17.png",
+      },
+     
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://s3-eu-west-1.amazonaws.com/tpd/logos/645ca753484ec7b9c3eac245/0x0.png",
+    role: "Artificial Intelligence (AI) Intern",
+    company: "INeuBytes",
+    date: "Mar 2025 - Present",
+    desc: "Developing AI-based solutions and working on machine learning algorithms.",
+    skills: [
+      "Python",
+      "Machine Learning",
+      "AI Models",
+      "TensorFlow",
+      "Data Analysis",
+    ],
+  },
+  {
+    id: 1,
+    img: "https://media.licdn.com/dms/image/v2/C4E0BAQHn10ja2QyoTw/company-logo_200_200/company-logo_200_200/0/1630649583584/microsoft_learn_student_ambassadors_logo?e=2147483647&v=beta&t=UAQ2xo64vR_BJDQHUF9hn5GHKH56STtCdKD7rd1sGs0",
+    role: "Microsoft Learn Student Ambassador",
+    company: "Microsoft Learn Student Ambassadors",
+    date: "Jan 2025 - Present",
+    desc: "Engaging with communities and sharing knowledge through various Microsoft platforms.",
+    skills: [
+      "Leadership",
+      "Microsoft Azure",
+      "Community Engagement",
+      "Community Influencer",
+    ],
+  },
+  {
+    id: 2,
+    img: "https://ali-cdn-diy-public.classplus.co/prod/Logo_1718989913215.png",
+    role: "Cyber Security and Ethical Hacking Intern",
+    company: "VaultOfCodes",
+    date: "Nov 2024 - Dec 2024",
+    desc: "Performed penetration testing and worked on security protocols for various applications.",
+    skills: [
+      "Kali Linux",
+      "Networking",
+      "Python","Nmap",
+      "Burp Suite","SQLMap","Tenable Nessus",
+     "Hashcat",
+    ],
+  },
+  {
+    id: 3,
+    img: "https://media.licdn.com/dms/image/v2/C560BAQHbRc4KalclmA/company-logo_200_200/company-logo_200_200/0/1676715857373?e=2147483647&v=beta&t=XUYYqixCLqqWCiNLro3MdZH-mHvqFl3yJ5D9blVH_Mk",
+    role: "Cyber Security and Ethical Hacking Intern",
+    company: "TechnoHacks EduTech Official",
+    date: "Sep 2024 - Oct 2024",
+    desc: "Worked on vulnerability assessments and ethical hacking assignments.",
+    skills: [
+      "Vulnerability Assessment",
+      "Nmap",
+      "Burp Suite",
+      "Python","Kali Linux","SQLMap",
+      "Cyber Security",
+    ],
+  },
+  {
+    id: 4,
+    img: "https://p.urbanpro.com/tv-prod/member/photo/4142883-large.jpg",
+    role: "Java Software Developer Intern",
+    company: "SDAC",
+    date: "Jul 2024 - Feb 2025",
+    desc: "Developed Java-based applications and contributed to backend services.",
+    skills: [
+      "Java", "Apache",
+      "MySQL",
+      "APIs", 
+      "Java Development",
+      
+     " Databases",
+      "HTML5",
+      
+      "Cascading Style Sheets (CSS)",
+      "Tomcat 5",
+      "XAMPP",
+      "Bootstrap (Framework)",
+      
+      "POJO",
+
+      "JavaServer Pages (JSP)",
+      
+      "JavaScript",
+     
+      "Software Development",
+    ],
+  },
+];
+
+
+export const education = [
+  {
+    id: 0,
+    img: "https://media.collegedekho.com/media/img/university/mumbai-university-result-2015.jpg",
+    school: "University of Mumbai",
+    date: "2022 - 2025",
+    grade: "Pursuing",
+    desc: "I am currently pursuing a Bachelor's degree in Information Technology at University of Mumbai. The course covers topics such as Web Development, Cybersecurity, Networking, Cloud Development and Database Management Systems.",
+    degree: "Bachelor of Science (B.Sc), Information Technology",
+  },
+  {
+    id: 1,
+    img: "https://www.mahahsscboard.in/LogoBord.png",
+    school: "Matruchhaya Junior College",
+    date: "2022",
+    grade: "70.00%",
+    desc: "I completed my Senior Secondary (XII) education from Matruchhaya Junior College under Maharashtra State Board Of Secondary And Higher Secondary Education in Commerce stream.",
+    degree: "Senior Secondary (XII), Commerce",
+  },
+  {
+    id: 2,
+    img: "https://yt3.googleusercontent.com/ytc/AIdro_nILJKhUN0p63f2qi71Tf3bFkPqqhBsV3lNbbwk1LEbSA=s900-c-k-c0x00ffffff-no-rj",
+    school: "ST PAUL'S HIGH SCHOOL",
+    date: "2020",
+    grade: "75.00%",
+    desc: "I completed my Secondary (X) education from ST PAUL'S HIGH SCHOOL under Maharashtra State Board Of Secondary And Higher Secondary Education with a percentage of 75.00%.",
+    degree: "Secondary (X), Science",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "iCoder Frontend",
+    date: "Nov 2023",
+    description: "iCoder is a frontend website built with HTML, CSS, and Bootstrap for tech enthusiasts. It showcases various tech-related content in a user-friendly layout.",
+    image: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/icoder.png?alt=media&token=icoderfrontend",
+    tags: ["HTML", "Cascading Style Sheets (CSS)", "Bootstrap (Framework)"],
+    category: "web app",
+    github: "https://github.com/username/iCoderFrontend",
+    webapp: ""
+  },
+  {
+    id: 1,
+    title: "Brain Tumor Detection",
+    date: "Jan 2023 - Mar 2023",
+    description:
+      "Preprocessed and augmented the dataset to improve model accuracy, trained the model, created API using model and Python, and used React web app for the project's front end. Achievements: Achieved an accuracy of 99.2% to accurately detect brain tumors from medical images.",
+    image:
+      "https://github.com/rishavchanda/Brain-Tumor-Detection/raw/main/Readme_resource/Image2.png",
+    tags: ["Python", "Keras", "TensorFlow", "VGG16", "Pickle", "React"],
+    category: "machine learning",
+    github: "https://github.com/rishavchanda/Brain-Tumor-Detection",
+    webapp: "https://brain-tumor.netlify.app/",
+    member: [
+      {
+        name: "Rishav Chanda",
+        img: "https://avatars.githubusercontent.com/u/64485885?v=4",
+        linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+        github: "https://github.com/rishavchanda/",
+      },
+      {
+        name: "Upasana Chaudhuri",
+        img: "https://avatars.githubusercontent.com/u/100614635?v=4",
+        linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+        github: "https://github.com/upasana0710",
+      },
+    ],
+  },
+ 
+  {
+    id: 2,
+    title: "Foodie Console Based Chatbot",
+    date: "Sep 2024",
+    description:
+      "A Foodie Console Based Chatbot made using Python, XML, and AIML. It helps users find recipes and offers information on various food items. It also provides API integration for fetching recipes from external sources.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/foodie-chatbot.png?alt=media&token=foodiechatbot",
+    tags: ["Python (Programming Language)", "XML", "AIML", "API Development"],
+    category: "console app",
+    github: "https://github.com/mehtahrishi/Foodie-Inc.-Chatbot",
+    webapp: "",
+  },
+  {
+    id: 3,
+    title: "Tech Savvy Assistant Chatbot",
+    date: "Jan 2025",
+    description:
+      "A Savier for Developers, Engineers, Testers, and Hardware Individuals who get stuck in errors and issues in tools, software, hardware, and hardware components. She can help you with her intelligence.",
+    image:
+      "https://res.cloudinary.com/dfzqhhywm/image/upload/v1741232077/Screenshot_2025-03-06_090420_ffx1qk.png",
+    tags: ["React.js", "Chatbot Development", "LiveChat ChatBot", "OpenAI API"],
+    category: "web app",
+    github: "https://github.com/mehtahrishi/Tech-Savvy",
+    webapp: "https://tech-savvy-three.vercel.app/",
+  },
+
+  {
+    id: 4,
+    title: "Admin Dashboard Frontend",
+    date: "Jan 2025 - Jan 2025",
+    description: "A dummy dashboard just for the sake of UI.",
+    image: "https://github.com/BobsProgrammingAcademy/responsive-sales-dashboard/raw/master/images/large.png?raw=true", 
+    tags: ["HTML5", "Cascading Style Sheets (CSS)", "JavaScript"],
+    category: "web app",
+    github: "https://github.com/mehtahrishi/Dashboard",
+    webapp: "https://mehtahrishi.github.io/Dashboard/index.html",
+  }
+ 
+
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
