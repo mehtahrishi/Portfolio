e973bea3e4e303dc5050b9748e0384437b1395d3
Hrishi <mehtahrishi45@gmail.com>
2025-04-23 16:42:46 +0530

    update

--- src/data/constants.js
+++ src/data/constants.js
@@ -672,20 +672,3 @@ export const projects = [
 
 
 ];
-
-export const TimeLineData = [
-  { year: 2017, text: "Started my journey" },
-  { year: 2018, text: "Worked as a freelance developer" },
-  { year: 2019, text: "Founded JavaScript Mastery" },
-  { year: 2020, text: "Shared my projects with the world" },
-  { year: 2021, text: "Started my own platform" },
-];
-export const volunteer = [
-  {
-    organization: "Red Cross",
-    role: "Event Volunteer",
-    date: "June 2023 - Present",
-    description: "Assisted in organizing blood donation camps and awareness drives."
-  },
-  // Add more...
-];
